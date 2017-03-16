6dbdb23cb9cff71239e3c18785a8084a3e71fba2
Benjamin Kampmann <ben@gnunicorn.org>
2017-03-16 12:11:13 +0100

    Environ based Email Setup

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,6 +1,13 @@
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address => "127.0.0.1",
-  :port => 25,
-  :domain => Rails.application.domain,
-  :enable_starttls_auto => false,
+    :address              => "smtp.zoho.eu",
+    :port                 => 465,
+    :domain               => 'demokratie-in-bewegung.org',
+    :user_name            => ENV['SMTP_USERNAME'],
+    :password             => ENV['SMTP_PASSWORD'],
+	  :ssl                  => true,
+	  :tls                  => true,
+	  :enable_starttls_auto => true   
 }
+
+Rails.application.config.mailer_sender = ENV['MAILER_ADDRESS']
