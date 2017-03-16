e89e13a8ede7f5a96da69cae74c7c46e04d5f328
Benjamin Kampmann <ben@gnunicorn.org>
2017-03-16 14:29:28 +0100

    ensure we are using TLS

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -2,12 +2,12 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     :address              => "smtp.zoho.eu",
     :port                 => 465,
-    :domain               => 'demokratie-in-bewegung.org',
+    # :domain               => 'demokratie-in-bewegung.org',
     :user_name            => ENV['SMTP_USERNAME'],
     :password             => ENV['SMTP_PASSWORD'],
-	  :ssl                  => true,
-	  :tls                  => true,
-	  :enable_starttls_auto => true   
+	:ssl                  => true,
+	# :tls                  => true,
+	:enable_starttls_auto => true   
 }
 
 Rails.application.config.mailer_sender = ENV['MAILER_ADDRESS']
